4487bc15b28965f16c5503b3aa7244e8f9449749
ramilfitkulin <56922122+RamZe3@users.noreply.github.com>
2021-02-19 23:10:08 +0400

    done 1.1.1

---
+++ Task 1/1.1.1/1.1.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int a, b;
+
+            Console.WriteLine("Введите a: ");
+            str = Console.ReadLine();
+            a = int.Parse(str);
+
+            Console.WriteLine("Введите b: ");
+            str = Console.ReadLine();
+            b = int.Parse(str);
+
+            if (a > 0 && b > 0)
+            {
+                Console.WriteLine("Площадь равна: " + (a * b));
+            }
+            else
+            {
+                Console.WriteLine("Введены некорректные значения!");
+            }
+        }
+    }
+}
