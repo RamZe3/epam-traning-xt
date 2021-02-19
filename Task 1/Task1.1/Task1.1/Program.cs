a9b5d2cda70854e0d7be164535fdca49ca4ebcc4
ramilfitkulin <56922122+RamZe3@users.noreply.github.com>
2021-02-20 00:03:56 +0400

    done 1.1.1

---
+++ Task 1/Task1.1/Task1.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _1._1._1
+{
+    class Program
+    {
+        static void RECTANGLE()
+        {
+            string str;
+            int a, b;
+
+            Console.WriteLine("Введите a: ");
+            str = Console.ReadLine();
+            a = int.Parse(str);
+
+            Console.WriteLine("Введите b: ");
+            str = Console.ReadLine();
+            b = int.Parse(str);
+
+            if (a > 0 && b > 0)
+            {
+                Console.WriteLine("Площадь равна: " + (a * b));
+            }
+            else
+            {
+                Console.WriteLine("Введены некорректные значения!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            RECTANGLE();
+        }
+
+    }
+}
