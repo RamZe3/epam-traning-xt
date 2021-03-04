fdb253931278071761532583c8ea017c995e6e84
ramilfitkulin <56922122+RamZe3@users.noreply.github.com>
2021-03-04 17:16:00 +0400

    done 1.1.1 - 1.2.1

---
+++ Task 1/Task1.2/Task1.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1._2
+{
+    class Program
+    {
+        static void AVERAGES()
+        {
+            int countOfWords = 0;
+            string str = Console.ReadLine();
+            //string s = "wqea, asd,qwe";
+
+            //way to split from Microsoft forum
+            char[] separators = new char[] { ' ', ':', '.', ',', ';', '!', '?', '(', ')', '-', '"' };
+            string[] words = str.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string word in words)
+            {
+                countOfWords += word.Length;
+            }
+
+            int averageLength = countOfWords / words.Length;
+            Console.WriteLine("Средняя длинна слов: {0}", averageLength);
+        }
+        static void Main(string[] args)
+        {
+            AVERAGES();
+        }
+    }
+}
