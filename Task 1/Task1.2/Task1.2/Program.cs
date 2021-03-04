1e43ba816dd0d6cf9b01d51b2b31245e3464b0b2
ramilfitkulin <56922122+RamZe3@users.noreply.github.com>
2021-03-04 17:49:45 +0400

    done 1.2.1 - 1.2.2

--- Task 1/Task1.2/Task1.2/Program.cs
+++ Task 1/Task1.2/Task1.2/Program.cs
@@ -8,11 +8,11 @@ namespace Task1._2
 {
     class Program
     {
+        // Task 1.2.1
         static void AVERAGES()
         {
             int countOfWords = 0;
             string str = Console.ReadLine();
-            //string s = "wqea, asd,qwe";
 
             //way to split from Microsoft forum
             char[] separators = new char[] { ' ', ':', '.', ',', ';', '!', '?', '(', ')', '-', '"' };
@@ -26,9 +26,38 @@ namespace Task1._2
             int averageLength = countOfWords / words.Length;
             Console.WriteLine("Средняя длинна слов: {0}", averageLength);
         }
+
+        // Task 1.2.2
+        static void DOUBLER()
+        {
+            Console.Write("ВВОД 1: ");
+            string str1 = Console.ReadLine();
+            Console.Write("ВВОД 2: ");
+            string str2 = Console.ReadLine();
+
+            var sb = new StringBuilder();
+            for (int i = 0; i < str1.Length; i++)
+            {
+                foreach(char item in str2)
+                {
+                    if (str1[i] == item)
+                    {
+                        sb.Append(str1[i], 1);
+                        break;
+                    }
+                }
+                sb.Append(str1[i], 1);
+            }
+
+            Console.WriteLine("ВЫВОД: {0}", sb.ToString());
+
+        }
+
+        // Main
         static void Main(string[] args)
         {
-            AVERAGES();
+            DOUBLER();
+            
         }
     }
 }
