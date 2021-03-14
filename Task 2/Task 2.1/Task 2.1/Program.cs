265a42c2b4ccb6e8cd6d6d799406d25fccb4b1de
ramilfitkulin <56922122+RamZe3@users.noreply.github.com>
2021-03-14 12:52:31 +0400

    done 2.1.1 (alpha)

--- Task 2/Task 2.1/Task 2.1/Program.cs
+++ Task 2/Task 2.1/Task 2.1/Program.cs
@@ -6,12 +6,8 @@ namespace Task_2._1
     // Task 2.1.1
     class CustomString
     {
-        public char[] custom_string { get; set; }
-
-        public int GetLength()
-        {
-            return custom_string.Length;
-        }
+        private char[] custom_string { get; set; }
+        public int Length { get => this.custom_string.Length; }
 
         public CustomString()
         {
@@ -45,7 +41,8 @@ namespace Task_2._1
             return -1;
         }
 
-        public string ConvertToString()
+        override
+        public string ToString()
         {
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < custom_string.Length; i++)
@@ -55,16 +52,29 @@ namespace Task_2._1
             return sb.ToString();
         }
 
-        public void Append(string string_)
+        public char[] ConvertToArray()
         {
-            char[] strings = new char[string_.Length];
-            for (int i = 0; i < string_.Length; i++)
+            return custom_string;
+        }
+
+        public void Append(string addStr)
+        {
+            char[] answer = new char[custom_string.Length + addStr.Length + 1];
+            char[] addStrArr = new char[addStr.Length];
+            for (int i = 0; i < addStr.Length; i++)
             {
-                strings[i] = string_[i];
+                addStrArr[i] = addStr[i];
             }
-            char[] answer = new char[custom_string.Length + string_.Length + 1];
             custom_string.CopyTo(answer, 0);
-            strings.CopyTo(answer, custom_string.Length);
+            addStrArr.CopyTo(answer, custom_string.Length);
+            custom_string = answer;
+        }
+
+        public void Append(CustomString cs)
+        {
+            char[] answer = new char[custom_string.Length + cs.Length + 1];
+            custom_string.CopyTo(answer, 0);
+            cs.custom_string.CopyTo(answer, custom_string.Length);
             custom_string = answer;
         }
 
@@ -89,11 +99,17 @@ namespace Task_2._1
     {
         static void Main(string[] args)
         {
-            CustomString css = new CustomString("abc a");
-            Console.WriteLine(css.FindByValue('b'));
-            css.Append("123");
-            css.Append("1235");
-            Console.WriteLine(css.ConvertToString());
+            // Task 2.1.1
+            char[] arr = { 'd', 'e', 'f' };
+            CustomString cs1 = new CustomString("abc");
+            CustomString cs1 = new CustomString("abc");
+            CustomString cs2 = new CustomString(arr);
+            cs1.Append(cs2);
+            cs1.Append("ghi");
+            Console.WriteLine("ToString: {0}",cs1.ToString());
+            Console.WriteLine("ConvertToArray[0]: {0}", cs1.ConvertToArray()[0]);
+            Console.WriteLine("Compare: {0}", cs1.Compare(cs2));
+            Console.WriteLine("Length: {0}", cs1.Length);
         }
     }
 }
