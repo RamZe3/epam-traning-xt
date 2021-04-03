41e190a54eb1ed8613d373912640c0727dadcf89
ramilfitkulin <56922122+RamZe3@users.noreply.github.com>
2021-04-03 14:51:08 +0400

    add Task 3.2

---
+++ Task 3/Task 3.2/Task 3.2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Task_3._2
+{
+
+    public class DynamicArray<T>
+    {
+        private T[] array;
+        public int Length { get; private set; } = 0;
+        public int Capacity { get => array.Length;}
+
+        public DynamicArray()
+        {
+            array = new T[8];
+        }
+
+        public DynamicArray(int size)
+        {
+            array = new T[size];
+        }
+
+        public DynamicArray(ICollection<T> collection)
+        {
+            array = new T[collection.Count];
+
+            foreach (var item in collection)
+            {
+                array[Length] = item;
+                Length++;
+            }
+        }
+
+        public void Add(T obj)
+        {
+            if (Length == Capacity)
+            {
+                // TODO
+                T[] _array = new T[array.Length*2];
+                array.CopyTo(_array, Length);
+                array = _array;
+            }
+            array[Length] = obj;
+            Length++;
+        }
+
+        /*public bool Remove(T item)
+        {
+            int index;
+            for (int i = 0; i < Length; i++)
+            {
+                if (array[i] == item)
+                {
+
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }*/
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(34);
+            list.Add(4);
+            list.Remove(0);
+
+            DynamicArray<int> dynamicArray = new DynamicArray<int>(list);
+            Console.WriteLine(dynamicArray.Capacity);
+            dynamicArray.Add(3);
+            Console.WriteLine(dynamicArray.Capacity);
+
+        }
+    }
+}
