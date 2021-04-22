ca723c09b47c4c734413662ed27b14e8e96c4e1f
ramilfitkulin <56922122+RamZe3@users.noreply.github.com>
2021-04-22 23:19:30 +0400

    done 4.1 (alpha)

--- Task 4/Task 4.1/Task 4.1/Program.cs
+++ Task 4/Task 4.1/Task 4.1/Program.cs
@@ -9,58 +9,77 @@ namespace Task_4._1
 {
     public class FileManager
     {
-        private DirectoryInfo directoryInfo;
         private string reserveDirectory;
-        private string reserveDirectoryWithDate;
+        private string directory;
+        private string date { get => DateTime.Now.ToString(@"yyyy-MM-dd-HH-mm"); }
+
         private string indirectory;
-        public string directory { get; set; }
+        private string outdirectory;
+
         public FileManager(string directory)
         {
             // TODO try
-            directoryInfo = new DirectoryInfo(directory);
             this.directory = directory;
-            //directoryFiles = Directory.GetFiles(directory);
-
             reserveDirectory = Directory.GetDirectoryRoot(directory) +
-                directoryInfo.Name + @"Reserve\";
+                 new DirectoryInfo(directory).Name + @"Reserve\";
             Directory.CreateDirectory(reserveDirectory);
-        }
-
-        public string GetDate()
-        {
-            return DateTime.Now.ToString(@"yyyy-MM-dd-HH-mm");
         }
 
         public void CreateReserveDirectory()
         {
             indirectory = directory;
-            reserveDirectoryWithDate = reserveDirectory + @"\" + GetDate() + @"\";
-            Directory.CreateDirectory(reserveDirectoryWithDate);
-            CopyFilesFromDir(directory, reserveDirectoryWithDate);
-            ForeachDirsCopy(directory, reserveDirectoryWithDate, reserveDirectoryWithDate);
+            outdirectory = reserveDirectory + @"\" + date + @"\";
+            Directory.CreateDirectory(outdirectory);
+            CopyFilesFromDir(indirectory, outdirectory);
+            ForeachDirsCopy(indirectory, outdirectory);
+        }
+        public void ComeBack()
+        {
+            indirectory = ShowReserveDirs();
+            outdirectory = directory;
+            CopyFilesFromDir(indirectory, outdirectory);
+            ForeachDirsCopy(indirectory, outdirectory);
         }
 
-        public void ForeachDirsCopy(string indirectory, string outdirectory, string mainoutdirectory)
+        private string ShowReserveDirs()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(reserveDirectory);
+            var ReserveDirs = directoryInfo.GetDirectories();
+            Console.WriteLine("Выберете дату для отката:");
+            Console.WriteLine("   Год-Месяц-День-Час-Мин");
+            for (int i = 0; i < ReserveDirs.Length; i++)
+            {
+                Console.WriteLine(i + 1 + ": " + ReserveDirs[i]);
+            }
+
+            string str = Console.ReadLine();
+            int choice;
+            int.TryParse(str, out choice);
+            return ReserveDirs[choice - 1].FullName + @"\";
+        }
+
+        private void ForeachDirsCopy(string indirectory, string outdirectory)
         {
             DirectoryInfo directoryInfo = new DirectoryInfo(indirectory);
             foreach (var dir in directoryInfo.GetDirectories())
             {
                 Directory.CreateDirectory(outdirectory + dir + @"\");
-                CopyFilesFromDir(indirectory + dir + @"\", mainoutdirectory);
-                ForeachDirsCopy(indirectory+ dir +  @"\", outdirectory + dir + @"\", mainoutdirectory);
+                CopyFilesFromDir(indirectory + dir + @"\", this.outdirectory);
+                ForeachDirsCopy(indirectory+ dir +  @"\", outdirectory + dir + @"\");
             }
 
         }
 
-        public void CopyFilesFromDir(string directory, string outdirectory)
+        private void CopyFilesFromDir(string directory, string outdirectory)
         {
+            // TODO EX
             DirectoryInfo dirInfo = new DirectoryInfo(directory);
             string dirWithoutMain = directory.Replace(indirectory, "");
             try
             {
                 foreach (var file in dirInfo.GetFiles("*.txt"))
                 {
-                    Console.WriteLine(outdirectory + dirWithoutMain + file.Name);
+                    //Console.WriteLine(outdirectory + dirWithoutMain + file.Name);
                     File.Copy(file.FullName, outdirectory + dirWithoutMain +  file.Name, true);
                 }
 
@@ -70,30 +89,6 @@ namespace Task_4._1
                 throw ex;
             }
         }
-
-        public void ComeBack_()
-        {
-            DirectoryInfo directoryInfo = new DirectoryInfo(reserveDirectory);
-            int count = 1;
-            Console.WriteLine("Выберете дату копирования:");
-            Console.WriteLine("   Год-Месяц-День-Час-Мин");
-            foreach (var dir in directoryInfo.GetDirectories())
-            {
-                Console.WriteLine( count + ": " + dir);
-                count++;
-            }
-        }
-
-        public void ComeBack(string nameOfDir)
-        {
-            indirectory = nameOfDir;
-            reserveDirectoryWithDate = nameOfDir;
-            CopyFilesFromDir(reserveDirectoryWithDate, directory);
-            ForeachDirsCopy(reserveDirectoryWithDate, directory, directory);
-        }
-
-
-
     }
 
 
@@ -102,50 +97,9 @@ namespace Task_4._1
     {
         static void Main(string[] args)
         {
-            string dir = @"E:\EpamTestDir\";
-
-            var dir_ = Directory.GetFiles(dir);
-            var dir__ = Directory.GetDirectories(dir);
-            var asd = Directory.GetFileSystemEntries(dir);
-            //Console.WriteLine(dir__[0]);
-            foreach (var file in dir_)
-            {
-                //Console.WriteLine(File.ReadAllText(file));
-                // TODO
-                //Console.WriteLine(file);
-                //Console.WriteLine(Path.GetFileName(file));
-                //Console.WriteLine(Path.GetExtension(file));
-            }
-            //Console.WriteLine();
-            foreach (var dir1 in dir__)
-            {
-                //Console.WriteLine(dir1);
-                DirectoryInfo directoryInfo = new DirectoryInfo(dir1);
-                //Console.WriteLine(Directory.GetDirectoryRoot(dir1));
-                //Console.WriteLine(directoryInfo.Name);
-                //Console.WriteLine(Path.GetDirectoryName(dir1));
-                Console.WriteLine();
-            }
-            //var str = File.ReadAllText(path);
-            //Console.WriteLine(str);
-            FileManager fileManager = new FileManager(dir);
-            //fileManager.CopyFiles();
-            //Console.WriteLine('\n');
-            //Console.WriteLine('\n');
-            //Console.WriteLine(Directory.GetAccessControl(dir));
-
+            FileManager fileManager = new FileManager(@"E:\EpamTestDir\");
             fileManager.CreateReserveDirectory();
-
-
-
-            string ert = "asdqasd";
-            string asdf = ert.Replace("asdq", "");
-            //Console.WriteLine(asdf);
-            //Console.WriteLine();
-            //Console.WriteLine(fileManager.GetDate());
-            //fileManager.ComeBack_();
-            //fileManager.CopyFilesFromDir(@"E:\EpamTestDirReserve\2021-04-22-21-12\", @"E:\EpamTestDir\");
-            //fileManager.ComeBack(@"E:\EpamTestDirReserve\2021-04-22-21-34\");
+            fileManager.ComeBack();
 
         }
     }
