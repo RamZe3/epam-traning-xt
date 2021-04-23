ce4af23f58c61f7abb4af1dcaa7a53e065e5e0bd
ramilfitkulin <56922122+RamZe3@users.noreply.github.com>
2021-04-23 15:21:32 +0400

    done 4.1

---
+++ Task 4/Task 4.1/Task 4.1/FileManager.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4._1
+{
+    public class FileManager
+    {
+        private string reserveDirectory;
+        private string directory;
+        private string date { get => DateTime.Now.ToString(@"yyyy-MM-dd-HH-mm"); }
+
+        private string indirectory;
+        private string outdirectory;
+
+        public FileManager(string directory)
+        {
+            this.directory = directory;
+            try
+            {
+                reserveDirectory = Directory.GetDirectoryRoot(directory) +
+                 new DirectoryInfo(directory).Name + @"Reserve\";
+                Directory.CreateDirectory(reserveDirectory);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("\nВведен некорректный адрес!\n");
+            }
+        }
+
+        public void ChangeDirectory(string directory)
+        {
+            this.directory = directory;
+            try
+            {
+                reserveDirectory = Directory.GetDirectoryRoot(directory) +
+                 new DirectoryInfo(directory).Name + @"Reserve\";
+                Directory.CreateDirectory(reserveDirectory);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nВведен некорректный адрес!\n");
+            }
+        }
+
+        public void CreateReserveDirectory()
+        {
+            try
+            {
+                indirectory = directory;
+                outdirectory = reserveDirectory + @"\" + date + @"\";
+                Directory.CreateDirectory(outdirectory);
+                CopyFilesFromDir(indirectory, outdirectory);
+                ForeachDirsCopy(indirectory, outdirectory);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("\nОшибка исполнения! (Введен некорректный адрес)\n");
+            }      
+        }
+        public void ComeBack()
+        {
+            try
+            {
+                indirectory = ShowReserveDirs();
+
+                if (indirectory == null)
+                {
+                    return;
+                }
+
+                outdirectory = directory;
+                CopyFilesFromDir(indirectory, outdirectory);
+                ForeachDirsCopy(indirectory, outdirectory);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nОшибка исполнения! (Введен некорректный адрес)\n");
+            }   
+        }
+
+        private string ShowReserveDirs()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(reserveDirectory);
+            var ReserveDirs = directoryInfo.GetDirectories();
+            Console.WriteLine("Выберете дату для отката:");
+            Console.WriteLine("   Год-Месяц-День-Час-Мин");
+            Console.WriteLine("0: Выход");
+            for (int i = 0; i < ReserveDirs.Length; i++)
+            {
+                Console.WriteLine(i + 1 + ": " + ReserveDirs[i]);
+            }
+
+            string str = Console.ReadLine();
+            int.TryParse(str, out int choice);
+
+            if (choice == 0)
+            {
+                return null;
+            }
+
+            try
+            {
+                return ReserveDirs[choice - 1].FullName + @"\";
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("\nНе существует данного решения!\n");
+                return ShowReserveDirs();
+            }
+        }
+
+        private void ForeachDirsCopy(string indirectory, string outdirectory)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(indirectory);
+            foreach (var dir in directoryInfo.GetDirectories())
+            {
+                Directory.CreateDirectory(outdirectory + dir + @"\");
+                CopyFilesFromDir(indirectory + dir + @"\", this.outdirectory);
+                ForeachDirsCopy(indirectory + dir + @"\", outdirectory + dir + @"\");
+            }
+
+        }
+
+        private void CopyFilesFromDir(string directory, string outdirectory)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(directory);
+            string dirWithoutMain = directory.Replace(indirectory, "");
+
+            foreach (var file in dirInfo.GetFiles("*.txt"))
+            {
+                File.Copy(file.FullName, outdirectory + dirWithoutMain + file.Name, true);
+            }
+        }
+    }
+}
--- Task 4/Task 4.1/Task 4.1/Program.cs
+++ Task 4/Task 4.1/Task 4.1/Program.cs
@@ -7,97 +7,18 @@ using System.Threading.Tasks;
 
 namespace Task_4._1
 {
-    public class FileManager
-    {
-        private string reserveDirectory;
-        private string directory;
-        private string date { get => DateTime.Now.ToString(@"yyyy-MM-dd-HH-mm"); }
-
-        private string indirectory;
-        private string outdirectory;
-
-        public FileManager(string directory)
-        {
-            // TODO try
-            this.directory = directory;
-            reserveDirectory = Directory.GetDirectoryRoot(directory) +
-                 new DirectoryInfo(directory).Name + @"Reserve\";
-            Directory.CreateDirectory(reserveDirectory);
-        }
-
-        public void CreateReserveDirectory()
-        {
-            indirectory = directory;
-            outdirectory = reserveDirectory + @"\" + date + @"\";
-            Directory.CreateDirectory(outdirectory);
-            CopyFilesFromDir(indirectory, outdirectory);
-            ForeachDirsCopy(indirectory, outdirectory);
-        }
-        public void ComeBack()
-        {
-            indirectory = ShowReserveDirs();
-            outdirectory = directory;
-            CopyFilesFromDir(indirectory, outdirectory);
-            ForeachDirsCopy(indirectory, outdirectory);
-        }
-
-        private string ShowReserveDirs()
-        {
-            DirectoryInfo directoryInfo = new DirectoryInfo(reserveDirectory);
-            var ReserveDirs = directoryInfo.GetDirectories();
-            Console.WriteLine("Выберете дату для отката:");
-            Console.WriteLine("   Год-Месяц-День-Час-Мин");
-            for (int i = 0; i < ReserveDirs.Length; i++)
-            {
-                Console.WriteLine(i + 1 + ": " + ReserveDirs[i]);
-            }
-
-            string str = Console.ReadLine();
-            int choice;
-            int.TryParse(str, out choice);
-            return ReserveDirs[choice - 1].FullName + @"\";
-        }
-
-        private void ForeachDirsCopy(string indirectory, string outdirectory)
-        {
-            DirectoryInfo directoryInfo = new DirectoryInfo(indirectory);
-            foreach (var dir in directoryInfo.GetDirectories())
-            {
-                Directory.CreateDirectory(outdirectory + dir + @"\");
-                CopyFilesFromDir(indirectory + dir + @"\", this.outdirectory);
-                ForeachDirsCopy(indirectory+ dir +  @"\", outdirectory + dir + @"\");
-            }
-
-        }
-
-        private void CopyFilesFromDir(string directory, string outdirectory)
-        {
-            // TODO EX
-            DirectoryInfo dirInfo = new DirectoryInfo(directory);
-            string dirWithoutMain = directory.Replace(indirectory, "");
-            try
-            {
-                foreach (var file in dirInfo.GetFiles("*.txt"))
-                {
-                    //Console.WriteLine(outdirectory + dirWithoutMain + file.Name);
-                    File.Copy(file.FullName, outdirectory + dirWithoutMain +  file.Name, true);
-                }
-
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-    }
-
-
-
     class Program
     {
         static void Main(string[] args)
         {
-            FileManager fileManager = new FileManager(@"E:\EpamTestDir\");
+            // with Ex
+            FileManager fileManager = new FileManager(@"фE:\EpamTestDir\");
+            fileManager.CreateReserveDirectory();
+            fileManager.ComeBack();
+
+            // main
+
+            fileManager.ChangeDirectory(@"E:\EpamTestDir\");
             fileManager.CreateReserveDirectory();
             fileManager.ComeBack();
 
