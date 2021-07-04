63f680f16139797c1a39dfd7f529cc3c37bd60bc
ramilfitkulin <56922122+RamZe3@users.noreply.github.com>
2021-07-05 01:54:56 +0400

    done Task 8.1 (beta)

---
+++ Task 8/WebTask1/WebTask1/Models/GenerationId.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace WebTask1.Models
+{
+    public static class GenerationId
+    {
+        public static string GenerateHash()
+        {
+            var bytes = new byte[16];
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(bytes);
+            }
+
+            string hash = BitConverter.ToString(bytes).Replace("-", "").ToLower();
+
+            return hash;
+        }
+    }
+}
